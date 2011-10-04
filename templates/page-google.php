95ba42bb54f2b327a627e03c62e3ccc1c10ebc95
Richard Cyganiak <richard@cyganiak.de>
2011-10-04 21:40:26 +0100

    remove Subscribed Links feature, Google dropped support for it

--- templates/page-google.php
+++ templates/page-google.php
@@ -1,8 +1,12 @@
   <div class="page-about">
     <h1>Google custom search result subscription</h1>
-    <p>If you use Google, then you can subscribe to custom results from
+    <p><strong>This feature is no longer available because Google
+      has <a href="http://searchengineland.com/google-shutters-fast-flip-sidewiki-aardvark-subscribed-links-91554">dropped its Subscribed Links
+      feature</a>.</strong></p>
+    <p><strike>If you use Google, then you can subscribe to custom results from
       prefix.cc. This means that a search for “foaf” will prominently show
-      the FOAF namespace from prefix.cc among your usual search results.</p>
-    <p>You need to be logged into your Google Account for this to work.</p>
-    <p style="text-align: center"><a href="http://www.google.com/coop/trust/add?user=009704136215832986560&sig=__j_jZ3Ak0JOC8PKti6Ey8_Lam61U="><img src="http://www.google.com/coop/images/addourexpertisebadge.gif" class="subscribe-button" border="0" alt="Add our expertise to your Google search results"/></a></p>
+      the FOAF namespace from prefix.cc among your usual search
+      results.</strike></p>
+    <p><strike>You need to be logged into your Google Account for this to
+      work.</strike></p>
   </div>
