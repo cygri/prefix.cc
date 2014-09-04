0c38106f5434b3df20c105b65a7e9cc0b29f19ce
Markus Lanthaler <mark_lanthaler@gmx.net>
2014-09-04 22:15:48 +0200

    Do not escape forward slashes
    This requires PHP 5.4 to work.

--- templates/format/json.php
+++ templates/format/json.php
@@ -4,7 +4,7 @@ foreach ($namespaces as $ns) {
     if ($i++ > 0) echo ",";
     echo "\n  \"$ns[prefix]\": ";
     if ($ns['uri']) {
-        echo json_encode($ns['uri']);
+        echo json_encode($ns['uri'], JSON_UNESCAPED_SLASHES);
     } else {
         echo "null";
     }
